2d808972f614716d8652b02ec4a1d38ef6f66310
Jalen Parham <jalenparham97@gmail.com>
2020-04-12 13:36:34 -0400

    end of section 4

--- DatingApp-SPA/src/app/components/register/register.component.ts
+++ DatingApp-SPA/src/app/components/register/register.component.ts
@@ -4,10 +4,9 @@ import { AuthService } from 'src/app/services/auth.service'
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css'],
+  styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-  @Input() values: any
   @Output() cancelRegister = new EventEmitter()
   user: any = {}
 
